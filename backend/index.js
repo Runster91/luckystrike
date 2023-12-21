463d8e7fb7a400628795a707bcde637b5335eb56
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-12-20 18:50:50 -0600

    db start

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
+
+
 const express = require ("express")
 const cors = require ("cors")
 const mongoose = require("mongoose")
+const dotenv = require('dotenv').config()
 
 const app = express ()
 app.use (cors())
@@ -8,6 +11,29 @@ app.use(express.json({limit : "10mb"}))
 
 const PORT = process.env.PORT || 8080
 
+//MONGODB cnonnection
+console.log(process.env.MONGODB_URL)
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.BASE_URL_DB_PRODUCCION).then(()=>console.log("Connected to Database")).catch(
+    (err)=>console.log(err)
+)
+
+//schema
+const userSchema=mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: String,
+    confirmpassword: String,
+    image: String   
+})
+ //
+ const model  =mongoose.model("user",userSchema)
+
+//API
 app.get("/", (req,res)=>{
     res.send("Server is running")
 })
